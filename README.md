5c8fe79049d65308db74b8557557f0dcef67b5e1
MrShwhale <wapatmore@gmail.com>
2024-06-05 14:51:25 -0700

    Finish most of the recommender, and a note.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # scp-recommender
 A simple recommender which aims to assist users of the SCP wiki in finding new articles to read.
 
-# TODO
-Fix it sometimes looking for name in blank area; Could be Xpath error?
+# The absolute state of this project
+So, I started this project as a way to learn a little bit about data science and make a cool thing at the end. I have learned a few lessons about collaborative filtering and the like which have gotten me to this point, which generates the similarities between users. However, something else I learned is that I should have done this in a completely different way, after learning the theoretical basis for machine learning. 
+
+I'm going to come back to this over the summer, and try to turn it into a fully functioning recommender with a web frontend. I might even host it somewhere. But it will probably have very few similarities to this project in terms of how it reccomends things. The webscraper will largely be the same, however (though it may support tag scraping and getting other information).
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import code
+import sys
 
 OUTPUT_DIR = "./output"
 PAGES_PATH = f"{OUTPUT_DIR}/pages.csv"
 RATINGS_PATH = f"{OUTPUT_DIR}/ratings.csv"
 USERS_PATH = f"{OUTPUT_DIR}/users.csv"
-RATING_MAP_PATH = f"{OUTPUT_DIR}/rating_map.pkl"
+# RATING_MAP_PATH = f"{OUTPUT_DIR}/rating_map.pkl"
+RATING_MAP_PATH = f"{OUTPUT_DIR}/short_rating_map.pkl"
 
 def build_pages_frame():
     print("Making pages frame")
@@ -18,26 +21,30 @@ def build_users_frame():
 def create_ratings_frame():
     # TODO move this into the spider finishing
     # Instead of a naive read, this should return a dataframe like this:
-    #        userId userId ...
-    # pageId rating rating
-    # pageId rating rating
+    #        pageId pageId ...
+    # userId rating rating
+    # userId rating rating
     # ...
     raw_ratings = pd.read_csv(RATINGS_PATH)
 
-    # Map that takes a uid then pid, and returns a rating
+    # Map that takes a pid then uid, and returns a rating
     upmap = {}
 
     print("Making upmap")
 
     for (_, uid, pid, rating) in raw_ratings.itertuples():
-        if uid not in upmap.keys():
-            upmap[uid] = {}
-        upmap[uid][pid] = rating
+        if pid not in upmap.keys():
+            upmap[pid] = {}
+
+        # Your votes are being detected
+        upmap[pid][uid] = int(rating)
 
     print("Upmap finished")
 
     # Creates a dataframe with index of pageId and columns of userId
-    final_frame = pd.DataFrame.from_dict(upmap)
+    # It is sparse since density is about 0.2%
+    final_frame = pd.DataFrame.from_dict(upmap, dtype=pd.SparseDtype("f2", np.nan))
+    print("Pickle-ing")
     final_frame.to_pickle(RATING_MAP_PATH)
 
     return final_frame
@@ -54,12 +61,33 @@ def build_ratings_frame():
 
     return final_frame
 
-def build_frames():
-    pages = build_pages_frame()
-    users = build_users_frame()
-    ratings = build_ratings_frame()
-    return (ratings, pages, users)
+def find_similar(df: pd.DataFrame, uid):
+    # For the row at index uid in this df, 
+    urow = df.loc[uid]
+    uid_mag = np.sqrt((urow ** 2).sum())
+    # Create a series of the reciprical of the magnitudes times the uid_mag
+    maglist = df.apply(lambda n: (np.sqrt((n ** 2).sum()) * uid_mag) ** -1, axis=1)
+
+    # Then multiply it all together
+    similarities = (df @ urow) * maglist
+    print(similarities)
+
+    return similarities
 
 if __name__ == "__main__":
-    (ratings, pages, users) = build_frames()
-    
+    # Your user id is 7904845 btw
+    ratings = build_ratings_frame()
+
+    # Who are we looking for
+    uid = 6657336
+
+    # Don't normalize ratings, we ball
+    # Create similarity matrix
+    similar_users = find_similar(ratings, uid)
+
+    # Sort similarity matrix
+    similar_users.sort_values(inplace=True, ascending=False) 
+
+    # Drop the value given uid
+    similar_users.drop(uid)
+    similar_users.head()
