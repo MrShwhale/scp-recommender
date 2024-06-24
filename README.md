3891e194bfdfc440c4d204f3fb7e73d9c62a808b
MrShwhale <wapatmore@gmail.com>
2024-06-24 02:07:21 -0700

    Finish basic recommendation system.

--- README.md
+++ README.md
@@ -3,9 +3,21 @@ A simple recommender which aims to assist users of the SCP wiki in finding new a
 
 # Issues
 The scraper only checks pages tagged with one of the 4 most popular formats: goi-format, scp, tale, hub.
-If something does not have one of these tags, it will not show up in the scraper
+If something does not have one of these tags, it will not show up in the scraper.
 
-# The absolute state of this project
-So, I started this project as a way to learn a little bit about data science and make a cool thing at the end. I have learned a few lessons about collaborative filtering and the like which have gotten me to this point, which generates the similarities between users. However, something else I learned is that I should have done this in a completely different way, after learning the theoretical basis for machine learning. 
+# To do
+## Optimize
+Consider saving certain dataframes to the disk, in case the server goes down (mostly for debug).
 
-I'm going to come back to this over the summer, and try to turn it into a fully functioning recommender with a web frontend. I might even host it somewhere. But it will probably have very few similarities to this project in terms of how it reccomends things. The webscraper will largely be the same, however (though it may support tag scraping and getting other information).
+Profile for memory and CPU usage.
+
+Look over basically every nontrivial pandas operation used.
+
+## Features
+Request recommendations with certain tags.
+
+Add a list of "novoted" pages, which will not be suggested.
+
+Add web components.
+
+Add fuzzy username search.
--- recommend.py
+++ recommend.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pandas as pd
-import scipy.stats
 from sklearn.metrics.pairwise import cosine_similarity
 
-# The minimum number of rating to consider someone's opinion
+# The minimum number of votes to consider someone's opinion
 MIN_RATINGS_THRESHOLD = 5
 
 # The maximum number of similar users to base the recommendations on
 MAX_SIMILAR_USERS = 10
 
-# Turn the ratings from a raw dict to a matrix
+# The maximum number of recommendations to show to the user
+MAX_RECOMMENDATIONS_LISTED = 10
+
+# Turn the ratings from a raw dict to a user-page matrix (rating map)
 def process_data(raw_ratings: pd.DataFrame) -> pd.DataFrame:
-    # Make this sparse
     rating_map = raw_ratings.pivot_table(index="userId", columns="pageId", values="rating", fill_value=0)
-    rating_map.astype(pd.SparseDtype(np.float32, 0), copy=False)
+
+    # Make the above sparse (will not help if the original is too big to hold it)
+    # BUG For some reason, actually making this a sparse matrix will crash things on line 24 when it runs out of memory
+    # My best guess is that .abs() creates a full version of the matrix for some reason
+    # rating_map = rating_map.astype(pd.SparseDtype(np.float32, 0), copy=True)
 
     # Filter out people with very few entries
     # Absolute value thing ONLY WORKS since the downvotes and upvotes are both 1
@@ -27,41 +32,55 @@ def normalize_data(rating_map: pd.DataFrame) -> None:
     # This could be a critical issue but if you knew what you were doing you wouldn't be here at all
     # So, change the average rating to be the mean but with a single 0 rating added
 
-    # TODO check if it is faster to add/remove a fake page with rating 0 for all users and use mean
     norm_factor = rating_map.sum(axis=1) / (rating_map.count(axis=1) + 1)
     rating_map = rating_map.subtract(norm_factor, axis="rows")
 
+# Makes a map which shows how similar each user is to each other user
 def generate_similarity(rating_map: pd.DataFrame) -> pd.DataFrame:
-    # TODO this probably has a better way to do it
-    # That sentence probably has a better way to do it too
-    similar_users = cosine_similarity(rating_map)
-    similar_users = pd.DataFrame(similar_users, index=rating_map.index, columns=rating_map.index)
-    # Only the index has the uid, not the columns as in PPMC
+    # TODO There is probably a better way to do this
+    # Specifically, one where only the similarity to the user is generated rather than everyone's similarities
+    similar_users = pd.DataFrame(cosine_similarity(rating_map), index=rating_map.index, columns=rating_map.index)
+
+    # Drop all times where the user is compared to itself
+    similar_users = similar_users.loc[similar_users.index != similar_users.columns]
     return similar_users
 
+# Returns a sorted DataFrame with all of the most similar_users to a given user
 def find_similar_users(similarity_matrix: pd.DataFrame, uid: int) -> pd.DataFrame:
-    similar = similarity_matrix.loc[uid]
-    similar.drop(uid, inplace=True)
-    similar.sort_values(inplace=True, ascending=False)
+    # TODO why is this a dataframe and not series?
+    similar = similarity_matrix.loc[uid].sort_values(inplace=False, ascending=False)
     return similar
 
-def get_recomendations(similarity_matrix: pd.DataFrame, rating_map: pd.DataFrame, uid: int):
+# Return the recommendations for the given user based on similar users and what those users liked
+def get_recomendations(similarity_matrix: pd.DataFrame, rating_map: pd.DataFrame, uid: int) -> pd.Series:
     # Consider saving an old copy of rating_map to the disk rather than keeping both in memory
-    read_pages = rating_map[uid]
+    read_pages = rating_map[rating_map.index == uid]
 
     # Drop all the pages the user hasn't read
-    # https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
-    read_pages = read_pages.loc[(read_pages!=0).all(axis=1)]
-    
+    read_pages = read_pages.loc[uid, read_pages.iloc[0] != 0]
+
     # Get pages read by the most similar users
     # TODO refactor or at least understand this line, you copied it in an airport
-    similar_pages = rating_map[rating_map.index.isin(similarity_matrix.head(10).index)]
-    similar_pages = similar_pages.loc[(similar_pages!=0).all(axis=1)]
+    most_similar_users = similarity_matrix.head(10)
+    similar_pages = rating_map[rating_map.index.isin(most_similar_users.index)]
+
+    # This might be VERY slow, idk
+    similar_pages = similar_pages.loc[:, similar_pages.any(axis=0)]
 
     # Remove all pages the user has read from the recommendations
-    similar_pages = similar_pages.drop(read_pages.columns, axis=1, inplace=True, errors="ignore")
+    similar_pages.drop(read_pages.index, axis=1, inplace=True, errors="ignore")
 
-    print(similar_pages)
+    # Simple dot weight algorithm
+    # Could run into the issue that with multiple people
+    def dot_weight(votes):
+        return most_similar_users.values @ votes
+
+    # More complex weighting that takes slightly reduces "popularity bias"
+    # def whale_weight(votes):
+    #     return most_similar_users.values @ votes
+
+    similar_pages = similar_pages.apply(dot_weight, axis=0)
+    similar_pages.sort_values(ascending=False, inplace=True)
 
     return similar_pages
 
@@ -91,48 +110,34 @@ if __name__ == "__main__":
     similar_users = generate_similarity(rating_map)
 
     # At this point the model is done. The rest is recommendations!
-
     # Get input from the user about what they want and for which user
     user_map = pd.read_csv("./output/users.csv")
     page_map = pd.read_csv("./output/pages.csv")
-    while (True):
-        # UID for test: 28829
-        print("Mode selection: ")
-        print("1. Get users similar to another user.")
-        print("2. Get recommendations for a user.")
-        print("3. Get username from uid.")
-        print("4. Get uid from username")
-        user_in = input()
-        if user_in == "1":
-            print("Enter your user id: ", end="")
-            user_in = input()
+
+    while True:
+        # Ask for the username
+        while True:
+            print("Username: ", end="")
+            username = input()
             try:
-                uid = int(user_in)
-                similar = find_similar_users(similar_users, uid)
-                print(similar)
+                uid = user_map.loc[user_map["userName"] == username]["userId"].iloc[0]
+                break
             except:
-                print("Couldn't find that uid")
-        elif user_in == "2":
-            try:
-                uid = int(user_in)
-                similar = find_similar_users(similar_users, uid)
-                recommendations = get_recomendations(similar, rating_map, uid)
-            except:
-                print("Couldn't find that uid")
-        elif user_in == "3":
-            print("Enter your user id: ", end="")
-            user_in = input()
-            try:
-                uid = int(user_in)
-                print(user_map.loc[user_map["userId"] == uid]["userName"].iloc[0])
-            except:
-                print("Couldn't find that uid")
-        elif user_in == "4":
-            print("Enter your user name: ", end="")
-            user_in = input()
-            try:
-                print(user_map.loc[user_map["userName"] == user_in]["userId"].iloc[0])
-            except:
-                print("Couldn't find that name")
-        else:
-            print("I do not know what that means.")
+                print("We can't find that username. Make sure you have the caps right and everything.")
+
+        print(f"Getting recommendations for {username}...")
+        try:
+            similar = find_similar_users(similar_users, uid)
+        except:
+            print(f"You must have at least {MIN_RATINGS_THRESHOLD} pages voted on to use this tool! Come back with more votes when this updates.")
+            continue
+
+        recommendations = get_recomendations(similar, rating_map, uid)
+        print("Recommendations:")
+
+        i = 0
+        for rec in recommendations.index:
+            if i >= MAX_RECOMMENDATIONS_LISTED:
+                break
+            print(f"{page_map.loc[page_map['pageId'] == rec]['title'].iloc[0]}")
+            i += 1
